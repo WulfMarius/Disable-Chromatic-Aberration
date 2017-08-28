476bf08ee465ea7e55bfd1d9a87caa1fbc05bb0a
WulfMarius <WulfMarius@users.noreply.github.com>
2017-08-28 17:47:03 +0200

    initial version

---
+++ DisableChromaticAberration/DisabledChromaticAberration.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+
+namespace DisableChromaticAberration
+{
+    class DisabledChromaticAberration
+    {
+        [HarmonyPatch(typeof(UniStormWeatherSystem), "Init")]
+        internal class UniStormWeatherSystemInitPatch
+        {
+            public static void Postfix(UniStormWeatherSystem __instance)
+            {
+                foreach (WeatherStateConfig eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+                {
+                    if (eachWeatherStateConfig == null)
+                    {
+                        continue;
+                    }
+
+                    DisableChromaticAberration(eachWeatherStateConfig.m_AfternoonColors);
+                    DisableChromaticAberration(eachWeatherStateConfig.m_DawnColors);
+                    DisableChromaticAberration(eachWeatherStateConfig.m_DuskColors);
+                    DisableChromaticAberration(eachWeatherStateConfig.m_MiddayColors);
+                    DisableChromaticAberration(eachWeatherStateConfig.m_MorningColors);
+                    DisableChromaticAberration(eachWeatherStateConfig.m_NightColors);
+                }
+            }
+
+            private static void DisableChromaticAberration(TODStateConfig config)
+            {
+                if (config == null)
+                {
+                    return;
+                }
+
+                config.m_VignettingChromaticAberration = 0;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Disable-Chromatic-Aberration
+
+
+This is a mod for **The Long Dark** by Hinterland Games Studio, Inc.
+
+
+It disables Chromatic Aberration in all quality settings.
+
+Requires the [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller) to run.
