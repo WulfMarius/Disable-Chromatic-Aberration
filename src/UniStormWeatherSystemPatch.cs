44d148ecfe19bf28ae283c8d8541ac010ca15136
WulfMarius <WulfMarius@users.noreply.github.com>
2017-08-31 12:20:40 +0200

    - renamed class file - removed obsolete class

---
+++ src/UniStormWeatherSystemPatch.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+
+namespace DisableChromaticAberration
+{
+    [HarmonyPatch(typeof(UniStormWeatherSystem), "Init")]
+    internal class UniStormWeatherSystemInitPatch
+    {
+        public static void Postfix(UniStormWeatherSystem __instance)
+        {
+            foreach (WeatherStateConfig eachWeatherStateConfig in __instance.m_WeatherStateConfigs)
+            {
+                if (eachWeatherStateConfig == null)
+                {
+                    continue;
+                }
+
+                DisableChromaticAberration(eachWeatherStateConfig.m_AfternoonColors);
+                DisableChromaticAberration(eachWeatherStateConfig.m_DawnColors);
+                DisableChromaticAberration(eachWeatherStateConfig.m_DuskColors);
+                DisableChromaticAberration(eachWeatherStateConfig.m_MiddayColors);
+                DisableChromaticAberration(eachWeatherStateConfig.m_MorningColors);
+                DisableChromaticAberration(eachWeatherStateConfig.m_NightColors);
+            }
+        }
+
+        private static void DisableChromaticAberration(TODStateConfig config)
+        {
+            if (config == null)
+            {
+                return;
+            }
+
+            config.m_VignettingChromaticAberration = 0;
+        }
+    }
+}
